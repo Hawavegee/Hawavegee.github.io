94b914243e52ef93d0394c5646bf8ef68f47af05
hawavegee <hawavegee@163.com>
2019-07-10 09:44:59 +0800

    Testing

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-addEventListener('fetch', ev => {
+self.addEventListener('fetch', ev => {
+    console.log('Handling fetch event for', event.request.url);
     ev.respondWith(new Response('OJJK'));
 });
